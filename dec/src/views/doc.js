ba31586886cfe766d90a315d8433af92dd7db2d3
Hetu Patel <hetu200211@gmail.com>
2021-06-17 22:30:44 +0530

    pdf change

--- dec/src/views/doc.js
+++ dec/src/views/doc.js
@@ -7,14 +7,23 @@ const doc = new PDFDocument();
 // Pipe its output somewhere, like to a file or HTTP response
 // See below for browser usage
 doc.pipe(fs.createWriteStream("output.pdf"));
-
-doc.fontSize(30).text("EXPERIMENT", 220, 5);
-doc.fontSize(20).text("Aim:", 0, 40);
-doc.fontSize(20).text("Apparatus:", 0, 80);
-
-doc.image("ZAID.jpg", {
-  fit: [250, 300],
+doc.fontSize(20).text("Name_student:",20,20);
+doc.fontSize(20).text("Date:",450,20);
+doc.fontSize(30).text("EXPERIMENT", 220, 50);
+doc.fontSize(20).text("Aim:", 20, 100);
+doc.fontSize(20).text("Apparatus:", 20, 180);
+doc.fontSize(20).text("Diagram:", 20, 300);
+doc.rect(8,10,595,770);
+doc.stroke()
+doc.rect(20,330,570,0);
+doc.stroke()
+doc.image("ZAID.jpg",150,350 ,{
+  fit: [300, 400],
   align: "center",
   valign: "center",
 });
+doc.addPage();
+doc.rect(8,10,595,770);
+doc.stroke();
+doc.fontSize(20).text("Theory:",20,40);
 doc.end();
